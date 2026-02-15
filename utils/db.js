9808003f4fafacabe9e003317246b4ace23ebd11
Rohit kumar yadav <rohitkuyada@gmail.com>
2026-02-15 15:04:55 +0530

    fix

--- utils/db.js
+++ utils/db.js
@@ -1,56 +1,27 @@
 import mongoose from 'mongoose';
 
-const MONGO_URI = process.env.MONGO_URI;
-
-if (!MONGO_URI) {
-    throw new Error('Please define the MONGO_URI environment variable');
-}
-
-/** * Global is used here to maintain a cached connection across hot-reloads 
- * in development and multiple worker threads during build.
- */
-let cached = global.mongoose;
-
-if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null };
-}
-
 export const connectDB = async () => {
-    // If we already have a connection, return it
-    if (cached.conn) {
-        return cached.conn;
-    }
-
-    if (!cached.promise) {
-        const opts = {
-            bufferCommands: false,
-        };
-
-        cached.promise = mongoose.connect(MONGO_URI, opts).then((mongoose) => {
-            console.log('\n--- MongoDB connected successfully ---');
-            return mongoose;
-        });
-    }
-
     try {
-        cached.conn = await cached.promise;
-    } catch (e) {
-        cached.promise = null;
-        console.error('Error connecting to MongoDB: ', e);
-        throw e;
+        if (mongoose.connection.readyState === 0) {
+            await mongoose.connect(process.env.MONGO_URI);
+            console.log('MongoDB connected successfully');
+        } else {
+            // console.log('MongoDB already connected');
+        }
     }
-
-    return cached.conn;
+    catch (error) {
+        console.error('Error connecting to MongoDB: ', error);
+        process.exit(1);
+    }
 }
-
 
 export const disconnectDB = async () => {
     try {
         if (mongoose.connection.readyState === 1) {
             await mongoose.disconnect();
-            console.log('\nMongoDB disconnected successfully');
+            console.log('MongoDB disconnected successfully');
         } else {
-            console.log('\nMongoDB already disconnected');
+            console.log('MongoDB already disconnected');
         }
     }
     catch (error) {
